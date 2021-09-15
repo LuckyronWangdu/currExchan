35c62edbcc5d9a1cba4bb82e6ac62bb36c0bad08
Lokesh Kumar <luckyronwangdu@gmail.com>
2021-09-15 15:40:23 +0530

    Full Commit

---
+++ src/main/java/com/currExchan/currExchan/CurrExchanApplication.java
@@ -0,0 +1,12 @@
+package com.currExchan.currExchan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrExchanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrExchanApplication.class, args);
+	}
+}
